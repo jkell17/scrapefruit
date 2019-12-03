8084f814bca9b2ed6a64d9df91abfcaae1670972
jkell17 <kelleher.jimmy@gmail.com>
2019-12-03 14:15:31 -0500

    Move to Markdown

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import sys
 from pathlib import Path
 from typing import List
 
-from setuptools import setup  # type: ignore
+from setuptools import find_packages, setup  # type: ignore
 
 assert sys.version_info >= (3, 7, 0), "Requires Python 3.7+"
 
 CURRENT_DIR = Path(__file__).parent
 
 
-NAME = "scrapefruit"
+NAME = "scrapefruit-jfk"
 VERSION = "0.0.1"
 
 
@@ -19,5 +19,24 @@ def get_requirements() -> List[str]:
         return f.read().split("\n")
 
 
+def get_long_description() -> str:
+
+    with open(CURRENT_DIR / "README.md") as f:
+        return f.read()
+
+
 if __name__ == "__main__":
-    setup(name=NAME, version=VERSION, install_requires=get_requirements())
+
+    setup(
+        name=NAME,
+        version=VERSION,
+        author="Jimmy Kelleher",
+        author_email="kelleher.jimmy@gmail.com",
+        description="A microframework to build asynchronous webscrapers in Python",
+        long_description=get_long_description(),
+        long_description_content_type="text/markdown",
+        url="https://github.com/jkell17/scrapefruit",
+        packages=find_packages(),
+        install_requires=get_requirements(),
+        python_requires=">=3.7",
+    )
