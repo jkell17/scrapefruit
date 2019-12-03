320a1dae6d68523be8b844957547aa016209dfd0
jkell17 <kelleher.jimmy@gmail.com>
2019-12-03 14:41:56 -0500

    Remove jsonlines

--- scrapefruit/export.py
+++ scrapefruit/export.py
@@ -1,22 +1,17 @@
 from typing import Dict, List
 
-import jsonlines  # type: ignore
+import json  # type: ignore
 
 
 class Exporter:
     def __init__(self, output_file: str):
         self.file = output_file
-        self.writer = jsonlines.open(self.file, "w")
+        self.writer = open(self.file, "w")
 
     def write(self, item: Dict) -> None:
-        self.writer.write(item)
+        self.writer.write(json.dumps(item))
+        self.writer.write('\n')
+
 
     def shutdown(self) -> None:
         self.writer.close()
-
-    def get_output(self) -> List[str]:
-        result = []
-        with jsonlines.open(self.file) as reader:
-            for obj in reader:
-                result.append(obj)
-        return result
